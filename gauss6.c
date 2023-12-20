8cb143709db306db3efb5f1adfb1cbb39bb22962
hcw <hcw.chiu@gmail.com>
2023-12-20 17:02:43 +0800

    init

---
+++ gauss6.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void print_pAug(int **pAug, int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n+1;j++){
+            printf("%d ", pAug[i][j]);
+        }printf("\n");
+    }
+}
+
+void print_pAugT(int **pAug, int n){
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<n;j++){
+            printf("%d ", pAug[i][j]);
+        }printf("\n");
+    }
+}
+
+void print_x(int *x, int n){
+    printf("[ ");
+    for(int i=0;i<n;i++){
+        printf("%d ", x[i]);
+    }printf("] \n");
+}
+
+int gcd(int a, int b){
+    if(b==0){
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+int lcm_array2(int *arr){
+    int gcd_result = arr[0];
+    int lcm = 1;
+    int size = sizeof(arr) / sizeof(arr[0]);
+    for(int i=0;i<size+1;i++){
+        lcm *= arr[i];
+        gcd_result = gcd(arr[i], gcd_result);
+    }
+    lcm /= gcd_result;
+    return lcm;
+}
+
+void transpose2(int **mat1, int **mat2, int rows, int cols){
+    for(int row=0;row<rows;row++){
+        for(int col=0;col<cols;col++){
+            mat1[col][row] = mat2[row][col];
+        }
+    }
+}
+
+void eliminate2(int **mat, int index_row, int n){
+    int *temp = (int *)malloc(n * sizeof(int));
+    for(int i=0;i<n+1;i++){
+        temp[i] = mat[index_row][i];
+    }
+    for(int row=0;row<n;row++){
+        for(int col=0;col<n+1;col++){
+            mat[row][col] -= temp[col];
+        }
+    }
+    for(int i=0;i<n+1;i++){
+        mat[index_row][i] = temp[i];
+    }
+    free(temp);
+}
+
+void unify2(int **mat, int index, int n){
+    int _r = n, _c = n+1;
+    int **a_transpose = (int **)malloc(_c * sizeof(int *));
+    for(int i=0;i<_c;i++){
+        a_transpose[i] = (int *)malloc(_r * sizeof(int));
+    }
+
+    transpose2(a_transpose, mat, _r, _c);
+
+    int col_lcm;
+    int *factor = (int *)malloc(_r * sizeof(int));
+
+    col_lcm = lcm_array2(a_transpose[index]);
+    //printf("lcm: %d \n", col_lcm);
+    for(int cursor=0;cursor<_r;cursor++){
+            factor[cursor] = col_lcm / a_transpose[index][cursor];
+            //printf("factor: %d \n", factor[cursor]);
+        }
+    for(int row=index;row<_r;row++){
+        for(int col=0;col<_c;col++){
+            mat[row][col] *= factor[row];
+        }
+    }
+    for(int i=0;i<_c;i++){
+        free(a_transpose[i]);
+    }
+    free(a_transpose);
+    free(factor);
+}
+
+void merge(int n, int* pA, int* py, int **Aug){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            Aug[i][j] = *(pA + i*n + j);
+        }
+    }
+    for(int i=0;i<n;i++){
+        Aug[i][n] = py[i];
+    }
+}
+
+void solve_x(int **Aug, int n, int *x){
+    int *temp = (int *)malloc(n * sizeof(int));
+    for(int i=0;i<n;i++){
+        temp[i] = Aug[i][n];
+    }
+    for(int i=n-1;i>=0;i--){
+        for(int j=n-1;j>i;j--){
+            temp[i] -= Aug[i][j] * x[j];
+        }
+        x[i] = temp[i] / Aug[i][i];
+    }
+    free(temp);
+}
+
+int gaussian_(int n, int* pA, int* py, int **px){
+    int **Aug = (int **)malloc(n * sizeof(int *));
+    for(int i=0;i<n;i++){
+        Aug[i] = (int *)malloc((n+1) * sizeof(int));
+    }
+    merge(n, pA, py, Aug);
+    printf("Augmented: \n");
+    print_pAug(Aug, n);
+    for(int index=0;index<n;index++){
+        unify2(Aug, index, n);
+        eliminate2(Aug, index, n);
+    }
+    printf("After Elimination: \n");
+    print_pAug(Aug, n);
+    solve_x(Aug, n, *px);
+    printf("x: \n");
+    print_x(*px, n);
+
+}
+
+int main(){
+
+    int n = 3;
+    int b_y[3] = {6,17,34};
+    int B[][3] = {
+        {1,1,1},
+        {1,2,4},
+        {1,3,9}
+    };
+
+    int* b_x = malloc(n * sizeof(int));
+    gaussian_(n, (int *)B, b_y, &b_x);
+    free(b_x);
+    
+    return 0;
+}
---
+++ temp/gauss2.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int gaussian_elimination(int n, int *pA, int *py, int **px) {
+  // 检查参数
+  if (n <= 0 || pA == NULL || py == NULL || px == NULL) {
+    return -1;
+  }
+
+  // 动态分配内存
+  int **a = malloc(sizeof(int *) * n);
+  for (int i = 0; i < n; i++) {
+    a[i] = malloc(sizeof(int) * (n + 1));
+  }
+
+  // 复制矩阵
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n + 1; j++) {
+      a[i][j] = pA[i * (n + 1) + j];
+    }
+  }
+
+  // 使用高斯消去法
+  for (int i = 0; i < n - 1; i++) {
+    if (fabs(a[i][i]) < 1e-6) {
+      printf("Matrix is singular!\n");
+      return -1;
+    }
+    for (int j = i + 1; j < n; j++) {
+
+      int ratio = a[j][i] / a[i][i];
+
+      for (int k = 1; k <= n + 1; k++) {
+        a[j][k] -= ratio * a[i][k];
+      }
+    }
+  }
+
+  // 回代求解
+  int temp = a[n - 1][n] / a[n - 1][n - 1];
+  px[n - 1] = &temp;
+
+  for (int i = n - 2; i >= 0; i--) {
+    if(a[i][i] != 0){
+        px[i] = malloc(sizeof(int));
+        *px[i] = a[i][n];
+        for (int j = i + 1; j < n; j++) {
+            *px[i] -= a[i][j] * (*px[j]);
+        }
+        *px[i] /= a[i][i];
+    }else{
+        px[i] = &py[i];
+    }
+
+  }
+
+  // 释放内存
+  for (int i = 0; i < n; i++) {
+    free(a[i]);
+  }
+
+  free(a);
+
+  return 0;
+}
+
+int main() {
+  int n;
+  int *pA, *py;
+  int **px;
+
+  // 输入未知数个数
+  printf("Enter number of unknowns: ");
+  scanf("%d", &n);
+
+  // 分配内存
+  pA = malloc(sizeof(int) * n * (n + 1));
+  py = malloc(sizeof(int) * n);
+  px = malloc(sizeof(int *) * n);
+
+  // 输入增广矩阵
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n + 1; j++) {
+      printf("a[%d][%d] = ", i, j);
+      scanf("%d", &pA[i * (n + 1) + j]);
+    }
+  }
+
+  // 调用函数
+  if (gaussian_elimination(n, pA, py, px) != 0) {
+    return -1;
+  }
+
+  // 输出解
+  printf("\nSolution:\n");
+  for (int i = 0; i < n; i++) {
+    printf("x[%d] = %d\n", i + 1 , *px[i]);
+  }
+
+  // 释放内存
+  free(pA);
+  free(py);
+  free(px);
+
+  return 0;
+}
